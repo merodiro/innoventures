0b27d9bd06c18d03ee8b734411a674ce8c4b646b
Amr A. Mohamed <merodiro@gmail.com>
2018-08-25 20:50:55 +0200

    add icons background

--- components/WeProvide.js
+++ components/WeProvide.js
@@ -46,11 +46,14 @@ const Feature = styled.div`
   padding-bottom: 3em;
 `
 
+import icons from '../static/img/icons.png'
+// import circuit from '../static/img/circuit-board.svg'
+
 const Section = styled.div`
   padding: 3em 0;
   background-color: var(--dark);
-  background-image: url("data:image/svg+xml,%3Csvg width='40' height='40' viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 0h20v20H0V0zm10 17a7 7 0 1 0 0-14 7 7 0 0 0 0 14zm20 0a7 7 0 1 0 0-14 7 7 0 0 0 0 14zM10 37a7 7 0 1 0 0-14 7 7 0 0 0 0 14zm10-17h20v20H20V20zm10 17a7 7 0 1 0 0-14 7 7 0 0 0 0 14z' fill='%23343a40' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");
-`
+  background-image: url(${icons});
+  `
 
 const WeProvide = () => {
   return (
