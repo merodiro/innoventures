3234c4584f2997a5d31e18d9199ed3fae5215b92
Amr A. Mohamed <merodiro@gmail.com>
2018-08-26 00:54:18 +0200

    center nav text on phone

--- components/Navbar.js
+++ components/Navbar.js
@@ -31,49 +31,43 @@ class MyNavbar extends React.Component {
   }
   render() {
     return (
-      <div>
-        <Navbar color="dark" dark expand="md" fixed="top">
-          <NavbarBrand href="/">
-            <img
-              src={Logo}
-              className="img-fluid"
-              style={{ maxHeight: '3em' }}
-            />
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="#">
-                  <i className="fas fa-home" />
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#" className="bg-primary rounded">
-                  Startup Reactor
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#">Ventures</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Company
-                </DropdownToggle>
-                <DropdownMenu right className="text-center">
-                  <DropdownItem>Ideaspace</DropdownItem>
-                  <DropdownItem>About</DropdownItem>
-                  <DropdownItem>Team</DropdownItem>
-                  <DropdownItem>News</DropdownItem>
-                  <DropdownItem>Blog</DropdownItem>
-                  <DropdownItem>Careers</DropdownItem>
-                  <DropdownItem>Contact</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      <Navbar className="text-center" color="dark" dark expand="md" fixed="top">
+        <NavbarBrand href="/">
+          <img src={Logo} className="img-fluid" style={{ maxHeight: '3em' }} />
+        </NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="#">
+                <i className="fas fa-home" />
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#" className="bg-primary rounded">
+                Startup Reactor
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Ventures</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Company
+              </DropdownToggle>
+              <DropdownMenu right className="text-center">
+                <DropdownItem>Ideaspace</DropdownItem>
+                <DropdownItem>About</DropdownItem>
+                <DropdownItem>Team</DropdownItem>
+                <DropdownItem>News</DropdownItem>
+                <DropdownItem>Blog</DropdownItem>
+                <DropdownItem>Careers</DropdownItem>
+                <DropdownItem>Contact</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
     )
   }
 }
